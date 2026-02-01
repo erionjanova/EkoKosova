89036273bac5d7616a1e7cd545afa25ce2d4c57a
drinberiishaa <drinberisha85@gmail.com>
2026-02-02 00:08:58 +0100

    databaza

--- database/ekokosova.sql
+++ database/ekokosova.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Feb 01, 2026 at 10:49 PM
+-- Generation Time: Feb 01, 2026 at 11:05 PM
 -- Server version: 5.7.24
 -- PHP Version: 8.0.1
 
@@ -36,15 +36,6 @@ CREATE TABLE `contacts` (
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Dumping data for table `contacts`
---
-
-INSERT INTO `contacts` (`id`, `full_name`, `email`, `subject`, `message`, `created_at`) VALUES
-(1, 'Drin Berish', 'drin@gmail.com', 'Kontakti', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s.', '2026-02-01 19:33:30'),
-(2, 'Filan Fisteku', 'filanfisteku@gmail.com', 'Kontakt', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s.', '2026-02-01 19:37:41'),
-(3, 'Test', 'test@gmail.com', 'Kontakt', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s.', '2026-02-01 19:42:08');
-
 -- --------------------------------------------------------
 
 --
@@ -57,15 +48,6 @@ CREATE TABLE `quotes` (
   `autori` varchar(255) NOT NULL,
   `autor_img` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `quotes`
---
-
-INSERT INTO `quotes` (`id`, `thenje_text`, `autori`, `autor_img`) VALUES
-(1, 'Higjiena eshte gjysma e shendetit!', 'Drin Berisha', 'uploads/697faa9d04bef.jpg'),
-(2, 'Higjiena eshte gjysma e shendetit!', 'Filan Fisteku', 'uploads/697fabff2898c.png'),
-(3, 'Higjiena eshte gjysma e shendetit!', 'Test', 'uploads/697face3b28bd.png');
 
 -- --------------------------------------------------------
 
@@ -85,15 +67,6 @@ CREATE TABLE `reports` (
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Dumping data for table `reports`
---
-
-INSERT INTO `reports` (`id`, `user_id`, `name`, `email`, `city`, `type`, `description`, `photo`, `created_at`) VALUES
-(1, 1, 'Drin Berisha', 'drin@gmail.com', 'prishtina', 'ajri', 'Ka ndotje te ajrit', '1769974308_ajri-ndotur.jpg', '2026-02-01 19:31:48'),
-(2, 2, 'Filan Fisteku', 'test@gmail.com', 'prizren', 'ujit', 'Uji eshte i ndotur', '1769974582_uji-ndotur.jpg', '2026-02-01 19:36:22'),
-(3, 3, 'Test', 'test@gmail.com', 'mitrovice', 'tokes', 'Toka eshte e ndotur!', '1769974910_toka.jpeg', '2026-02-01 19:41:50');
-
 -- --------------------------------------------------------
 
 --
@@ -112,15 +85,6 @@ CREATE TABLE `users` (
   `reset_expires` datetime DEFAULT NULL,
   `password_changed_at` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`id`, `name`, `email`, `profile_pic`, `username`, `password`, `is_admin`, `reset_token`, `reset_expires`, `password_changed_at`) VALUES
-(1, 'Drin Berisha', 'drin@gmail.com', 'uploads/profile_1769974219.jpg', 'drin', '$2y$10$42pe2yEzIWw7o5N4XE8iIuGbM8870bPJ28U4vTAHZG2kkf0HFdY46', 1, NULL, NULL, NULL),
-(2, 'Filan Fisteku', 'filanfisteku@gmail.com', 'uploads/member.png', 'filan', '$2y$10$Dg4amGSZbVUy5cvqCzjzfueH1/adUESCIEXs7kCdza7FI7EN0ULyG', 0, NULL, NULL, NULL),
-(3, 'Test', 'test@gmail.com', 'uploads/member.png', 'test', '$2y$10$krR0NiF5NNhpVCvVhpl1xeg4iFH2bjgA9Y9zUd9fdLRgZKKOGvex2', 0, NULL, NULL, NULL);
 
 --
 -- Indexes for dumped tables
